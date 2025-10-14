b9472ca54f4f00625c6c2d73ffbfe537bc025aab
Soulberto Lorenzo <96021+slorenzot@users.noreply.github.com>
2025-10-14 17:18:13 -0500

    Initial Commit

---
+++ Saleforce Inspect/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Saleforce Inspect
+//
+//  Created by Soulberto Lorenzo on 14/10/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
